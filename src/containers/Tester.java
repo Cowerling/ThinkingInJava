700dfbd5215ae76d752f85c7c78357a5b21f6b8f
Cowerling <Cowerling@gmail.com>
2015-12-11 10:50:15 +0800

    modify from workspace

--- src/containers/Tester.java
+++ src/containers/Tester.java
@@ -7,7 +7,7 @@ import java.util.List;
  */
 public class Tester<C> {
     public static int fieldWidth = 8;
-    public static TestParam[] defaultParams = TestParam.array(10, 5000, 100, 5000, 1000, 5000, 1000, 500);
+    public static TestParam[] defaultParams = TestParam.array(10, 5000, 100, 5000, 1000, 5000, 10000, 500);
 
     protected C container;
 
