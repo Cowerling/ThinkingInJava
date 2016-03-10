022a3b845228e50435d138c73ff80274a516a70c
Cowerling <Cowerling@gmail.com>
2016-03-10 21:04:17 +0800

    annotation start, at workspace

---
+++ src/annotations/TestObjectCreate.java
@@ -0,0 +1,10 @@
+package annotations;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by dell on 2016/3/10.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface TestObjectCreate {}
---
+++ src/atunit/ClassNameFinder.java
@@ -0,0 +1,14 @@
+package atunit;
+
+import java.io.*;
+import java.util.*;
+import util.*;
+
+/**
+ * Created by dell on 2016/3/10.
+ */
+public class ClassNameFinder {
+    public static String thisClass(byte[] classBytes) {
+        Map<>
+    }
+}
