214fec65b295f267f9904791719edbdc24897863
Cowerling <Cowerling@gmail.com>
2016-03-10 22:41:38 +0800

    annotation at home

---
+++ src/atunit/AtUnit.java
@@ -0,0 +1,167 @@
+package atunit;
+
+import java.lang.reflect.*;
+import java.io.*;
+import java.util.*;
+import annotations.*;
+import util.*;
+
+/**
+ * Created by cowerling on 16-3-10.
+ */
+public class AtUnit implements ProcessFiles.Strategy {
+    static Class<?> testClass;
+    static List<String> failedTests = new ArrayList<String>();
+    static long testsRun = 0;
+    static long failures = 0;
+
+    public static void main(String[] args) throws Exception {
+        ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
+
+        new ProcessFiles(new AtUnit(), "class").start(args);
+
+        if(failures == 0)
+            System.out.println("OK (" + testsRun + " tests)");
+        else {
+            System.out.println("(" + testsRun + " tests");
+            System.out.println("\n>>> " + failures + " FAILURE" + (failures > 1 ? "S" : "") + " <<<");
+            for(String failedTest : failedTests)
+                System.out.println(" " + failedTest);
+        }
+    }
+
+    public void process(File classFile) {
+        try {
+            String className = ClassNameFinder.thisClass(BinaryFile.read(classFile));
+
+            if(!className.contains("."))
+                return;
+
+            testClass = Class.forName(className);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        TestMethods testMethods = new TestMethods();
+        Method creator = null, cleanup = null;
+
+        for(Method method : testClass.getDeclaredMethods()) {
+            testMethods.addIfTestMethod(method);
+
+            if(creator == null)
+                creator = checkForCreatorMethod(method);
+
+            if(cleanup == null)
+                cleanup = checkForCleanupMethod(method);
+
+            if(testMethods.size() > 0) {
+                if(creator == null) {
+                    try {
+                        if(!Modifier.isPublic(testClass.getDeclaredConstructor().getModifiers())) {
+                            System.out.println("Error: " + testClass + " default constructor must be public");
+                            System.exit(1);
+                        }
+                    } catch (NoSuchMethodException e) {
+
+                    }
+                }
+            }
+
+            System.out.println(testClass.getName());
+        }
+
+        for(Method method : testMethods) {
+            System.out.print("  . " + method.getName() + " ");
+
+            try {
+                Object testObject = createTestObject(creator);
+                boolean success = false;
+
+                try {
+                    if(method.getReturnType().equals(boolean.class))
+                        success = (Boolean)method.invoke(testObject);
+                    else {
+                        method.invoke(testObject);
+                        success = true;
+                    }
+                } catch (InvocationTargetException e) {
+                    System.out.println(e.getCause());
+                }
+
+                System.out.println(success ? "" : "(failed)");
+                testsRun++;
+
+                if(!success) {
+                    failures++;
+                    failedTests.add(testClass.getName() + ": " + method.getName());
+                }
+
+                if(cleanup != null)
+                    cleanup.invoke(testObject, testObject);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    static class TestMethods extends ArrayList<Method> {
+        void addIfTestMethod(Method method) {
+            if(method.getAnnotation(Test.class) == null)
+                return;
+
+            if(!(method.getReturnType().equals(boolean.class) || method.getReturnType().equals(void.class)))
+                throw new RuntimeException("@Test method must return boolean or void");
+
+            method.setAccessible(true);
+            add(method);
+        }
+    }
+
+    private static Method checkForCreatorMethod(Method method) {
+        if(method.getAnnotation(TestObjectCreate.class) == null)
+            return null;
+
+        if(!method.getReturnType().equals(testClass))
+            throw new RuntimeException("@TestObjectCreate must return instance of Class to be tested");
+
+        if((method.getModifiers() & Modifier.STATIC) < 1)
+            throw new RuntimeException("@TestObjectCreate must be static.");
+
+        method.setAccessible(true);
+        return method;
+    }
+
+    private static Method checkForCleanupMethod(Method method) {
+        if(method.getAnnotation(TestObjectCleanup.class) == null)
+            return null;
+
+        if(!method.getReturnType().equals(void.class))
+            throw new RuntimeException("@TestObjectCleanup must return void");
+
+        if((method.getModifiers() & Modifier.STATIC) < 1)
+            throw new RuntimeException("@TestObjectCleanup must be static.");
+
+        if(method.getParameterTypes().length == 0 || method.getParameterTypes()[0] != testClass)
+            throw new RuntimeException("@TestObjectCleanup must take an argument of the tested type.");
+
+        method.setAccessible(true);
+        return method;
+    }
+
+    private static Object createTestObject(Method creator) {
+        if(creator != null) {
+            try {
+                return creator.invoke(testClass);
+            } catch (Exception e) {
+                throw new RuntimeException("Couldn't run @TestObject (creator) method");
+            }
+        } else {
+            try {
+                return testClass.newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException("Couldn't create a test object. Try using a @TestObject method.");
+            }
+        }
+    }
+}
--- src/atunit/ClassNameFinder.java
+++ src/atunit/ClassNameFinder.java
@@ -9,6 +9,73 @@ import util.*;
  */
 public class ClassNameFinder {
     public static String thisClass(byte[] classBytes) {
-        Map<>
+        Map<Integer, Integer> offsetTable = new HashMap<Integer, Integer>();
+        Map<Integer, String> classNameTable = new HashMap<Integer, String>();
+
+        try {
+            DataInputStream dataInputStream = new DataInputStream(new ByteArrayInputStream(classBytes));
+
+            int magic = dataInputStream.readInt();
+            int minorVersion = dataInputStream.readShort();
+            int majorVersion = dataInputStream.readShort();
+            int constant_pool_count = dataInputStream.readShort();
+            int[] constant_pool = new int[constant_pool_count];
+
+            for(int i = 0; i < constant_pool_count; i++) {
+                int tag = dataInputStream.read();
+                int tableSize;
+
+                switch (tag) {
+                    case 1:
+                        int length = dataInputStream.readShort();
+                        char[] bytes = new char[length];
+                        for(int k = 0; k < length; k++)
+                            bytes[k] = (char)dataInputStream.read();
+                        String className = new String(bytes);
+                        classNameTable.put(i, className);
+                        break;
+                    case 5:
+                    case 6:
+                        dataInputStream.readLong();
+                        i++;
+                        break;
+                    case 7:
+                        int offset = dataInputStream.readShort();
+                        offsetTable.put(i, offset);
+                        break;
+                    case 8:
+                        dataInputStream.readShort();
+                        break;
+                    case 3:
+                    case 4:
+                    case 9:
+                    case 10:
+                    case 11:
+                    case 12:
+                        dataInputStream.readInt();
+                        break;
+                    default:
+                        throw new RuntimeException("Bad tag " + tag);
+                }
+            }
+
+            short access_flags = dataInputStream.readShort();
+            int this_class = dataInputStream.readShort();
+            int super_class = dataInputStream.readShort();
+
+            return classNameTable.get(offsetTable.get(this_class)).replace('/', '.');
+        } catch (Exception e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        if(args.length > 0) {
+            for(String arg : args)
+                System.out.println(thisClass(BinaryFile.read(arg)));
+        } else {
+            for(File classFile : Directory.walk(".", ".*\\.class"))
+                System.out.println(thisClass(BinaryFile.read(classFile)));
+        }
     }
 }
